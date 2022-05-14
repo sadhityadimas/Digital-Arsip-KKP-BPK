ee452058bde67b8f9835479a4023d3c55fa78db3
sadhityadimas <s.adhitya.dimas@gmail.com>
2022-05-14 13:27:07 +0700

    first commit

--- arsip_app.py
+++ arsip_app.py
@@ -4,8 +4,30 @@ from st_aggrid import AgGrid, GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode
 from PIL import Image
 import sqlite3
+from gsheetsdb import connect
 import warnings
 warnings.filterwarnings('ignore')
+
+#from here
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["https://docs.google.com/spreadsheets/d/1pW0_JJ3NuXDcuIFlQ88v8qPa-YtD1B9dOu-J-JDAyX4/edit#gid=0"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
+
+#to here
 
 bpk_icon = Image.open("asset/BPK.ico")
 LOGO_IMAGE = "asset/BPK.png"
